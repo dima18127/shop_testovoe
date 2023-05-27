3dac871f121bcfacc1cd081dffc3dfd0aa315115
dima18127 <98730001+dima18127@users.noreply.github.com>
2023-05-27 22:26:01 +0400

    CardsJs

--- js/main.js
+++ js/main.js
@@ -11,11 +11,9 @@ goTopBtn.addEventListener("click", goTop);
 function trackScroll() {
   // вычисляем положение от верхушки страницы
   const scrolled = window.pageYOffset;
-  console.log(window.pageYOffset);
-  
   // считаем высоту окна браузера
   const coords = document.documentElement.clientHeight;
-  console.log(coords);
+
   
   // если вышли за пределы первого окна
   if (scrolled > coords/2) {
@@ -53,21 +51,85 @@ function goTop() {
 // };
 // getDayInfo('01.01.2022')
 
+
+let ShopList = [
+  { model: 'Fred Perry Олимпийка' ,
+    type:  'Олимпийка',
+    date: '2022.06.15',
+    image: 'images/clothes/1.webp',
+  },
+  { model: 'Fred Perry Олимпийка' ,
+  type:  'Олимпийка',
+  date: '2022.01.23',
+  image: 'images/clothes/2.webp',
+},
+  { model: 'Fred Perry Олимпийка' ,
+  type:  'Олимпийка',
+  date: '2023.08.02',
+  image: 'images/clothes/3.webp',
+  }
+]
+let ShopList = [
+  { model: 'Fred Perry Олимпийка' ,
+    type:  'Олимпийка',
+    date: '2022.06.15',
+    image: 'images/clothes/1.webp',
+  },
+  { model: 'Fred Perry Олимпийка' ,
+  type:  'Олимпийка',
+  date: '2022.01.23',
+  image: 'images/clothes/2.webp',
+},
+  { model: 'Fred Perry Олимпийка' ,
+  type:  'Олимпийка',
+  date: '2023.08.02',
+  image: 'images/clothes/3.webp',
+  }
+]
+
+
+
+function ShowCatalog (){
+const catalog = document.querySelector('.catalog-wrap');
+    for (let ShopItem of ShopList ){
+      // cтруктура карточки
+      
+      const card = document.createElement('div'),
+      img = document.createElement('img'),
+      cardName = document.createElement('div'),
+      cardDate = document.createElement('div');
+      cardBtn = document.createElement('button')
+      catalog.append(card);
+      card.append(img);
+      card.append(cardName);
+      card.append(cardDate);
+      card.append(cardBtn);
+      // стили элементов
+      cardName.classList.add('card-name');
+      card.classList.add('card-wrapper');
+      cardName.classList.add('card-name');
+      cardDate.classList.add('card-date');
+      cardBtn.classList.add('btn');
+      //наполнение контентом
+      cardName.innerText = ShopItem['model']
+      cardBtn.innerText = 'Купить'
+      cardDate.innerText = getDayInfo(ShopItem['date']);
+      img.setAttribute('src', ShopItem['image'])
+      img.setAttribute('alt', 'img')
+    }
+  }
+  ShowCatalog();
+
 function getDayInfo(data) {
   let result;
   let dayOfWeek = ['воскресенье','понедельник' , 'вторник', 'среда', 'четверг', 'пятница', 'суббота', ]
   let monthNames = [
-      "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
-      "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+      "Января", "Февраля", "Марта", "Апреля", "Мая", "Июня",
+      "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"
   ]
-  // let date2 = new Date()
   let date = new Date(data);
-  result = dayOfWeek[date.getDay()] +" " + monthNames[date.getMonth()] + " " + date.getFullYear() + " года"
   let day = Math.ceil(date.getDate()/7);
-  console.log(day);
-  
-  console.log(result);
-  date.getMonth()
-  // console.log(date2.getDay());
+  return result = dayOfWeek[date.getDay()] +" " + day +" неделя " + monthNames[date.getMonth()] + " " + date.getFullYear() + " года";
 }
+
   
